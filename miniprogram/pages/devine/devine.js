6bb48b8dc7601b4ff569a00fa96c03e9b106e6f6
庄瑞莹 <liuqingyilan@qq.com>
2024-03-26 00:21:44 +0800

    feat:抽牌

--- miniprogram/pages/devine/devine.js
+++ miniprogram/pages/devine/devine.js
@@ -5,14 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    cardStyles: [],
+    
   },
-
+  getCardStyle:function(index){
+    const rotationAngle = (index-5)*3.6; // 递增角度
+    const style = {
+      width: '150rpx',
+      height: '300rpx',
+      'margin-left': '-115rpx',
+      'background-image': 'url("https://6c65-lets-play-4g9llnlf4dd7edd7-1304389115.tcb.qcloud.la/taro.png?sign=ed773529996b850108477026ec993f61&t=1711380518")',
+      'background-size': `cover`,
+      'background-position': `center`,
+      transform: `rotate(${rotationAngle}deg)`
+    };
+    return Object.keys(style).map(key => `${key}: ${style[key]};`).join(' ');
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    var items = []
+    for (let i = 0; i < 15; i++) {
+      const style = this.getCardStyle(i);
+      items.push(style)
+    }
+    this.setData({
+      cardStyles: items
+    });
   },
 
   /**
