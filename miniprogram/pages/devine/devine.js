d0e1cebf97e16b75f71d974b5aef441bb72393f5
庄瑞莹 <liuqingyilan@qq.com>
2024-04-12 15:24:07 +0800

    thanks for someone~

--- miniprogram/pages/devine/devine.js
+++ miniprogram/pages/devine/devine.js
@@ -6,23 +6,19 @@ Page({
    */
   data: {
     cardStyles: [],
+    move: [],
   },
 
   handleDivClick:function(e){
-    const idx = e.currentTarget.dataset.idx
-    const cardStyles = this.data.cardStyles
-    cardStyles[idx] = {
-      width: '150rpx',
-      height: '300rpx',
-      'margin-left': '-115rpx',
-      'background-image': 'url("https://6c65-lets-play-4g9llnlf4dd7edd7-1304389115.tcb.qcloud.la/taro.png?sign=ed773529996b850108477026ec993f61&t=1711380518")',
-      'background-size': `cover`,
-      'background-position': `center`,
-      'animation': `moveTop 5s steps(60, end) forwards`,
-    };
+    const idx = e.currentTarget.dataset.idx;
+    let m = wx.createAnimation({duration: 200});
+    m.translateY(-300).step();
+    m.translateX(0).step();
+    var moving = this.data.move
+    moving[idx] = m.export();
     this.setData({
-      cardStyles: cardStyles
-    });
+      move: moving
+    })
   },
 
   getCardStyle:function(index){
@@ -34,7 +30,8 @@ Page({
       'background-image': 'url("https://6c65-lets-play-4g9llnlf4dd7edd7-1304389115.tcb.qcloud.la/taro.png?sign=ed773529996b850108477026ec993f61&t=1711380518")',
       'background-size': `cover`,
       'background-position': `center`,
-      transform: `rotate(${rotationAngle}deg)`
+      transform: `rotate(${rotationAngle}deg)`,
+      'transition': `all 3s ease`,
     };
     return Object.keys(style).map(key => `${key}: ${style[key]};`).join(' ');
   },
@@ -43,12 +40,17 @@ Page({
    */
   onLoad(options) {
     var items = []
+    var moves = []
     for (let i = 0; i < 15; i++) {
       const style = this.getCardStyle(i);
       items.push(style)
     }
+    for(let i=0;i<15;i++){
+      moves.push(undefined)
+    }
     this.setData({
-      cardStyles: items
+      cardStyles: items,
+      move: moves
     });
   },
 
